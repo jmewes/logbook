1632359e3ae03b81b98f84d61c480300f3907c8f
Jan Mewes <jan.mewes@experimental-software.com>
2026-07-18 17:51:16 +0200

    fix: stabilize logbook entry archiving (#43)
    GH-32

--- core/archiving.go
+++ core/archiving.go
@@ -1,7 +1,9 @@
 package core
 
 import (
+	"errors"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/jmewes/logbook/config"
@@ -17,6 +19,10 @@ func Archive(configuration config.Configuration, sourcePath string) (string, err
 		sourceDirectoryPath, configuration.LogDirectory, configuration.ArchiveDirectory, 1,
 	)
 
+	if _, err := os.Stat(targetDirectoryPath); err == nil {
+		return "", errors.New("target directory already exists: " + targetDirectoryPath)
+	}
+
 	err = os.MkdirAll(targetDirectoryPath, 0777)
 	if err != nil {
 		return "", err
@@ -26,6 +32,36 @@ func Archive(configuration config.Configuration, sourcePath string) (string, err
 	if err != nil {
 		return "", err
 	}
+
+	// Verify that all files have been copied
+	sourceFiles := make(map[string]os.FileInfo)
+	err = filepath.Walk(sourceDirectoryPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			relPath, err := filepath.Rel(sourceDirectoryPath, path)
+			if err != nil {
+				return err
+			}
+			sourceFiles[relPath] = info
+		}
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+	for relPath, sourceInfo := range sourceFiles {
+		targetPath := filepath.Join(targetDirectoryPath, relPath)
+		targetInfo, err := os.Stat(targetPath)
+		if err != nil {
+			return "", err
+		}
+		if sourceInfo.Size() != targetInfo.Size() {
+			return "", errors.New("file size mismatch: " + relPath)
+		}
+	}
+
 	err = os.RemoveAll(sourceDirectoryPath)
 	return targetDirectoryPath, err
 }
