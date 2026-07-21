5fc8e0688679bb14a427adef7c99a9f7e31128f9
Jan Mewes <jan.mewes@experimental-software.com>
2026-07-21 09:49:38 +0200

    Add failing test

--- core/adding_test.go
+++ core/adding_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"github.com/jmewes/logbook/utils"
 )
 
 func TestAddLogEntry(t *testing.T) {
@@ -29,4 +31,41 @@ func TestAddLogEntry(t *testing.T) {
 	if len(allLogEntries) != 1 {
 		t.Errorf("AddLogEntry returned wrong number of entries")
 	}
+}
+
+// should add counter-suffix for duplicate titles
+func TestAddLogEntry_duplicate_title(t *testing.T) {
+	fixture := setup(t)
+	t.Cleanup(fixture.cleanup)
+	// Given a logbook entry with the title "Foo"
+	_, _ = AddLogEntry(fixture.tempDir, "Foo", time.Now())
+
+	// When another logbook entry with the title "Foo" gets created
+	entry, _ := AddLogEntry(fixture.tempDir, "Foo", time.Now())
+
+	// Then the directory for the second logbook entry has the suffix "_2"
+	dir := utils.SimpleDirectoryName(entry.Directory)
+	if dir != "foo_2" {
+		t.Errorf("actual: %v, expected: %v", dir, "foo_2")
+	}
+}
+
+func setup(t *testing.T) fixture {
+	tempDir, err := os.MkdirTemp("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return fixture{
+		cleanup: func() {
+			//goland:noinspection GoUnhandledErrorResult
+			os.RemoveAll(tempDir)
+		},
+		tempDir: tempDir,
+	}
+}
+
+type fixture struct {
+	cleanup func()
+	tempDir string
 }
